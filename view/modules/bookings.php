b7f0047f9baea50080ffd80fc10d59c9c488af46
Caamal Peech Ernesto Valentin <ernesto.valen.t@gmail.com>
2019-05-02 21:03:11 -0500

    calendar testing convert json works
    Ya funciona el calendario falta terminar y corregir errores menores.

--- view/modules/bookings.php
+++ view/modules/bookings.php
@@ -24,8 +24,6 @@ if(isset($_SESSION['lang'])){
 }else{
   include "view/languages/es.php";
 }
-
-
 # ======  End of Language validation  =======
 ?>
 
@@ -49,15 +47,24 @@ if(isset($_SESSION['lang'])){
   <?php include 'view/modules/header/header.php'; ?>
   <!--====  End of HEADER  ====-->
 
+
 <?php
-
-
-$dataJson = array();
-$getBookinsData = BookingController::getBookinsData($id);
-// echo "<br>getBookin Obtiene: <br>";
-// var_dump($getBookinsData);
+  $getBookinsData = BookingController::getBookinsData($id);
+  $jsonData = array();
+  foreach ($getBookinsData as $key => $value) {
+    $name=utf8_encode($value["tour_name"]);
+    $date = new DateTime($value["booking_date"].substr($value["schedule_start"],0,5).":00");
+    $fin = new DateTime($date->format("c"));
+    $fin->add(new DateInterval('PT4H3M2S'));
+    $fin->add(new DateInterval("PT".substr($value["tour_duration"],0,1)."H".substr($value["tour_duration"],2,2)."M"));
+    echo "title: ".$name.", start: ".$date->format("c").", end: ".$fin->format("c")."<br>";
+    $jsonData[] = array("title" => $name, "start" => $date->format("c"), "end" => $fin->format("c"));
+  }
+//echo json_encode($jsonData);
 
  ?>
+
+
 
   <!--===========================================
   =            RESERVATIONS CALENDAR            =
@@ -105,89 +112,58 @@ $getBookinsData = BookingController::getBookinsData($id);
 
 
 <div class="container">
-
-    <div class="row">
-      <div class="col-md-12">
-
-        <p>Aquí encontrarás la lista de tours que te han solicitado por turistas y viajeros, es necesario aceptarlos para poder darle seguimiento al tour que te han solicitado, y además de que podrás obtener información de contacto y sobre todo para verificar que el tour se hará, esto en la sección <a target="_blank" href="http://localhost/guids/generateinsurance/user/<?php echo $id; ?>">Generar seguro grupal</a></p>
-
-        <table id="mytours" class="table table-bordered table-striped dt-responsive nowrap">
-          <thead>
-            <tr>
-             <th>Tour</th>
-              <th>Reservación de</th>
-              <th>Asistirán</th>
-              <th>Fecha y hora</th>
-              <th>Acciones</th>
-
-            </tr>
-          </thead>
-
-          <tbody>
-            <?php
-                foreach($getBookinsData AS $row => $value){
-                  //generate json
-                  $name=utf8_encode($value["tour_name"]);
-                  $date=$value["booking_date"]." ".substr($value["schedule_start"],0,5).":00";
-                  $start=strtotime("Y-m-d H:i:s", $date);
-                  echo $name." -> ".$date." -> ".date("Y-m-d H:i:s",$start)." -> ".$end."<br>";
-                } ?>
-
-
-            <tr>
-              <td><?php echo utf8_encode($value["tour_name"]); ?></td>
-              <td><a href="<?php echo "mailto:".$value["bookin_email"]; ?>"> <?php echo $value["booking_name"]." ".$value["booking_lastname"]; ?></a></td>
-              <td><?php echo $value["booking_quantyty"]." de ".$value["tour_capacity"]; ?></td>
-              <td><?php echo $value["booking_date"]." ".$value["schedule_start"]; ?></td>
-              <td style="width:100px;">
-
-                      <a style="color: white"; data-toggle="modal" data-target="#acceptBookingModal<?php echo $value["booking_id"];?>" class="btn btn-info btn-xs">Aceptar</a>
-
-                  </td>
-
-            </tr>
-
-              <!-- Modal -->
-                <div class="modal fade" id="acceptBookingModal<?php echo $value["booking_id"];?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                  <div class="modal-dialog modal-dialog-centered" role="document">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLongTitle">¿Aceptar el tour <?php echo utf8_encode($value["tour_name"]);?> ?</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                      <form method="post">
-                      <div class="modal-body">
-                        <input type="hidden" name="booking_id" value="<?php echo $value["booking_id"] ?>">
-                        <p class="color-black-opacity-5">Después de aceptar podrás ver la la información de contacto, tendrás que validar el tour <?php echo utf8_encode($value["tour_name"]);?> , este tour lo solicita <?php echo $value["booking_name"]." ".$value["booking_lastname"]; ?></p>
-                      </div>
-                      <div class="modal-footer">
-                          <button style="color: white"; type="button" class="btn btn-warning" data-dismiss="modal">Ahora no</button>
-                          <!-- <button type="submit" class="btn btn-danger">Eliminar</button> -->
-                          <input type="submit" value="De acuerdo" id="btnupdate" class="btn btn-success py-2 px-4 text-white" >
-                               <?php
-                                $acceptTour = new BookingController();
-                                $acceptTour->acceptTour();
-
-                               ?>
-                      </div>
-                      </form>
-                    </div>
-                  </div>
-                </div>
-
-                <!-- End of Modal -->
-            <?php //} ?>
-          </tbody>
-        </table>
-
-
-
-      </div>
-
+  <div class="row">
+    <div class="col-md-12">
+    <p>Aquí encontrarás la lista de tours que te han solicitado por turistas y viajeros, es necesario aceptarlos para poder darle seguimiento al tour que te han solicitado, y además de que podrás obtener información de contacto y sobre todo para verificar que el tour se hará, esto en la sección <a target="_blank" href="http://localhost/guids/generateinsurance/user/<?php echo $id; ?>">Generar seguro grupal</a></p>
+    <table id="mytours" class="table table-bordered table-striped dt-responsive nowrap">
+      <thead>
+        <tr>
+         <th>Tour</th>
+          <th>Reservación de</th>
+          <th>Asistirán</th>
+          <th>Fecha y hora</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Nombre</td>
+          <td><a href="">Persona</a></td>
+          <td>x de N</td>
+          <td>Fecha</td>
+          <td style="width:100px;">
+                  <a style="color: white"; data-toggle="modal" data-target="#acceptBookingModal" class="btn btn-info btn-xs">Aceptar</a>
+              </td>
+        </tr>
+      </tbody>
+    </table>
     </div>
+  </div>
 </div>
+<!-- Modal -->
+<div class="modal fade" id="acceptBookingModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">¿Aceptar el tour </h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form method="post">
+      <div class="modal-body">
+        <input type="hidden" name="booking_id" value="">
+        <p class="color-black-opacity-5">Después de aceptar podrás ver la la información de contacto, tendrás que validar el tour  , este tour lo solicita </p>
+      </div>
+      <div class="modal-footer">
+          <button style="color: white"; type="button" class="btn btn-warning" data-dismiss="modal">Ahora no</button>
+          <input type="submit" value="De acuerdo" id="btnupdate" class="btn btn-success py-2 px-4 text-white" >
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
+
 <!--====  End of MY RESERVATIONS  ====-->
 
 
@@ -215,14 +191,6 @@ $getBookinsData = BookingController::getBookinsData($id);
 
 <!--====  End of SCRIPTS  ====-->
 
-
-
-  <?php
-    $json_encode="Tours Zona arqueológica";
-    $json_encode_two="2019-04-12T22:30:00";
-    $json_encode_three="2019-04-12T23:30:00";
-   ?>
-
   <script>
       document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
@@ -238,76 +206,12 @@ $getBookinsData = BookingController::getBookinsData($id);
       navLinks: true, // can click day/week names to navigate views
       editable: false,
       eventLimit: true, // allow "more" link when too many events
-      events: [
-        {
-          title:<?php echo json_encode($json_encode)?>,
-          start: <?php echo json_encode($json_encode_two)?>,
-          end: <?php echo json_encode($json_encode_three)?>,
-        },
-        {
-          title: 'Long Event',
-          start: '2019-04-07',
-          end: '2019-04-10'
-        },
-        {
-          groupId: 999,
-          title: 'Repeating Event',
-          start: '2019-04-09T16:00:00'
-        },
-        {
-          groupId: 999,
-          title: 'Repeating Event',
-          start: '2019-04-16T16:00:00'
-        },
-        {
-          title: 'Conference',
-          start: '2019-04-11',
-          end: '2019-04-13'
-        },
-        {
-          title: 'Meeting',
-          start: '2019-04-12T10:30:00',
-          end: '2019-04-12T12:30:00'
-        },
-        {
-          title: 'Lunch',
-          start: '2019-04-12T12:00:00'
-        },
-        {
-          title: 'Meeting',
-          start: '2019-04-12T14:30:00'
-        },
-        {
-          title: 'Happy Hour',
-          start: '2019-04-12T17:30:00'
-        },
-        {
-          title: 'Dinner',
-          start: '2019-04-12T20:00:00'
-        },
-        {
-          title: 'Birthday Party',
-          start: '2019-04-13T07:00:00'
-        },
-        {
-          title: 'Click for Google',
-          url: 'http://google.com/',
-          start: '2019-04-28'
-        }
-      ]
+      events: <?php echo json_encode($jsonData); ?>
     });
-
-
         calendar.render();
       });
 
-  </script>
 
-<!--====  End of SCRIPTS  ====-->
-
-
-
-   <script>
       $(document).ready(function() {
         $('#mytours').DataTable();
         } );
